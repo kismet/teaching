e64e41681bf82ad105a78ab7cffd0a96a2d0ed87
Stefano Lenzi <kismet.sl@gmail.com>
2025-02-06 23:51:23 +0100

    Update 04-SleepyChildren.c codice errato caricato

--- posix/04-SleepyChildren.c
+++ posix/04-SleepyChildren.c
@@ -7,35 +7,36 @@
 //di un processo per aumentare la leggibilita del codice
 int sleepy_child(int n);
 
-//Queste costanti mi aiutano ad evitare la worst-practise dei 'Magic Number'
-const int SLEEPY_PARENT = 5;
-const int SLEEPY_CHILD[] = {7,12};
-
 int main(int argc,char *argv[]){
-    int child;
+    int nchildren;
     int myself = getpid();
+    int dead, status, child;
     printf("Tutto nasce con %d\n",myself);
-    child = fork();
-    if (child == 0){
-        exit(sleepy_child(SLEEPY_CHILD[0]));
+    printf("Quanti processi vuoi creare\n -> ");
+    scanf("%d",&nchildren);
+    int nsleep[nchildren];
+    
+    for(int i=0;i<nchildren;i++){
+        int seconds;
+        printf("Quanti deve aspettare il processo %d\n -> ",(i+1));
+        scanf("%d",&seconds);
+        nsleep[i]=seconds;
     }
-    printf("%d: Tutto bene, la fork funziona ha creato il primo figlio  %d\n",myself,child);
-
-    child = fork();
-    if (child == 0){
-        exit(sleepy_child(SLEEPY_CHILD[1]));
-    }
-    printf("%d: Tutto bene, la fork funziona ha creato il secondo figlio  %d\n",myself,child);
-
-    printf("%d: Dopo tutte queste fork mi è venuto sonno..\n", myself);
-    sleep(SLEEPY_PARENT);
-    printf("%d: %d secondi mi sono bastati, ora sono sveglio!\n",myself, SLEEPY_PARENT);
-
-    int statusAlpha,statusBeta;
-    int deadAlpha = wait(&statusAlpha);
-    int deadBeta = wait(&statusBeta);
-    printf("%d: %d e' terminato con codice %d\n",myself,deadAlpha,statusAlpha);
-    printf("%d: %d e' terminato con codice %d\n",myself,deadBeta,statusBeta);
+    
+    
+    for(int i=0;i<nchildren;i++){
+        child = fork();
+        if (child == 0){
+            exit(sleepy_child(nsleep[i]));
+        }
+    }    
+    
+    for(int i=0;i<nchildren;i++){
+        dead = wait(&status);
+        printf("%d: %d e' terminato con codice %d\n",myself,dead,status);
+    }     
+    
+    return 0;
 }
 
 int sleepy_child(int n){
